9fdf3628231a4cd636a02587cffc86f6a03805e3
hclarke77 <clarkehayden0@gmail.com>
2019-02-28 20:22:42 -0600

    refresh button w/o functionality and method for uploading files needed

--- pyAudioAnalysis/www/index.py
+++ pyAudioAnalysis/www/index.py
@@ -2,15 +2,21 @@ import os
 from flask import Flask, flash, request, redirect, url_for, render_template, send_from_directory
 from werkzeug.utils import secure_filename
 
+TEMPLATES_AUTO_RELOAD = True
+
 UPLOAD_FOLDER = './uploads'
 ALLOWED_EXTENSIONS = set(['wav'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# @app.route('/')
-# def homepage():
-#     return render_template('index.html')
+
+@app.route('/uploads')
+def autoload(filename):
+    if filename != '':
+        return render_template('index.html', name = filename)
+
+
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
@@ -20,7 +26,9 @@ def uploaded_file(filename):
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
+       
+            
+    
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
@@ -39,5 +47,5 @@ def upload_file():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return redirect(url_for('uploaded_file',
                                     filename=filename))
+        
     return render_template('index.html')
-    
